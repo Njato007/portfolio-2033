ee51e927fa4fe70c101d61dcf30e880b53f47e69
Njato007 <njatotianafiononana@gmail.com>
2023-02-15 16:16:17 +0300

    Third Update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,23 +8,35 @@ document.body.onscroll = (e) => {
 
 const observer = new IntersectionObserver(function(observables) {
     observables.forEach(observable => {
+        if (observable.isIntersecting) {
+            observable.target.classList.add('observed');
+            observer.unobserve(observable.target);
+        }
+    });
+}, {
+    root: null,
+    threshold: .25,
+    rootMargin: '0px'
+});
+
+const sections = document.querySelectorAll('section');
+sections.forEach(section => observer.observe(section));
+
+const MenuObserver = new IntersectionObserver((observables) => {
+    observables.forEach(observable => {
         const menuItem = document.querySelector(`a[href='#${observable.target.id}']`)
                         || document.createElement('a');
         if (observable.isIntersecting) {
             menuItem.classList.add('active');
-            if (!observable.target.classList.contains('observed'))
-                observable.target.classList.add('observed');
-            // observer.unobserve(observable.target);
         } else {
             menuItem.classList.remove('active');
         }
     });
 }, {
-    threshold: [0.5]
+    threshold: .5
 });
 
-const sections = document.querySelectorAll('section');
-sections.forEach(section => observer.observe(section));
+sections.forEach(section => MenuObserver.observe(section));
 
 /* SCROLL INTO VIEW BY LINK */
 // document.querySelectorAll("a[href^='#']").forEach(anchor => {
