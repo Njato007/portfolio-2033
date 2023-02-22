d6bb35a751ef1788d18707f34e419404c1578877
Njato007 <njatotianafiononana@gmail.com>
2023-02-22 10:04:58 +0300

    fixed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -45,3 +45,42 @@ window.addEventListener('scroll', () => {
     }
     activateMenu();
 });
+
+
+function sendEmail() {
+    const e = event;
+    e.preventDefault();
+    const name_form = document.getElementById('name');
+    const email_form = document.getElementById('email');
+    const message_form = document.getElementById('message');
+    Email.send({
+        Username: "njatosportfolio@gmail.com",
+        Host: "smtp.elasticemail.com",
+        Password: "DE798B4722A07CE34A8CE39F1AC4E505C79E",
+        To : 'njatotianafiononana@gmail.com',
+        From : 'njatosportfolio@gmail.com',
+        Subject : "Someone got in touch",
+        Body : `<html>
+            <body style="background-color: #f5f5f5; padding: 2em">
+                <div style="width: 500px; background: #fff; padding: 2em; border: 3px solid #24aea9; margin: 0 auto;">
+                    <h2 style="color: #24aea9;">From Portfolio</h2>
+                    <p class="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 14px;"><b style="color: #20303a;">Name:</b> ${name_form.value}</p>
+                    <p class="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 14px;"><b style="color: #20303a;">Email:</b> ${email_form.value}</p>
+                    <p class="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 14px; white-space: pre;"><b style="color: #20303a;">Message:</b> <br> ${message_form.value.replace(/\n/g, '<br>\n')}</p>
+                    <hr>
+                    <p class="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 14px;">Date: ${new Date().toLocaleDateString()}</p>
+                </div>
+            </body>
+        </html>`
+    }).then(
+        message => {
+            const Form = document.forms[0];
+            Form.classList.add('success');
+            e.target.reset();
+            message_form.style.height = "";
+            setTimeout(() => {
+                Form.classList.remove('success')
+            }, 5000);
+        }
+    ).catch(err => alert(err));
+}
